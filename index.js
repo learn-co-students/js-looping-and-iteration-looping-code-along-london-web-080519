2ae300aae3d56637576206a1ce61c87755457d37
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-14 11:29:48 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(cards) {
+    const thanks = [];
+    for(let i=0; i<cards.length; i++) {
+        thanks.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return thanks;
+};
+
+
+function countdown(myNum) {
+    let i = myNum;
+    while (myNum > -1) {
+        console.log(myNum--);
+    }
+};
