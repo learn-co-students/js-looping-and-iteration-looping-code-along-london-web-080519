0c899ef41d31a041a4b50c809e7486763438580e
github username <github email address>
2019-09-15 20:44:41 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import { createReadStream } from "fs"
+
 // Code your solutions in this file
+function writeCards(names, event){
+    let cardArray = [];
+    for (let i = 0; i < names.length; i++) {
+        cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return(cardArray);  
+}
+
+function countdown(num){
+    while (num >=0){
+        console.log(num--); 
+    }
+}
